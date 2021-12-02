a6a7ceea03c3a512ba40f9b54e4fd429248e5273
Esteban Ramos Coronado <esteban@Mac-mini-de-Esteban.local>
2021-12-02 18:26:36 +0100

    feat(adding flat github action and removing scaffolding)

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,44 +3,34 @@
 	import '../app.css';
 </script>
 
-<Header>
-	<main>
-		<slot />
-	</main>
+<Header />
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
-
-	<style>
-		main {
-			flex: 1;
-			display: flex;
-			flex-direction: column;
-			padding: 1rem;
-			width: 100%;
-			max-width: 1024px;
-			margin: 0 auto;
-			box-sizing: border-box;
-		}
-
-		footer {
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-			align-items: center;
-			padding: 40px;
-		}
-
-		footer a {
-			font-weight: bold;
-		}
-
-		@media (min-width: 480px) {
-			footer {
-				padding: 40px 0;
-			}
-		}
-	</style>
-</Header>
 <slot />
+
+<footer>
+	<p>
+		Realizado por <a href="http://www.estebanramos.work" target="_blank" rel="noopener noreferrer"
+			>Esteban Ramos</a
+		> en Barcelona
+	</p>
+</footer>
+
+<style>
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 40px 0;
+		}
+	}
+</style>
