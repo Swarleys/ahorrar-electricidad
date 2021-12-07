c2029bc98b30c4781280208502109bc5557da0ae
Esteban Ramos Coronado <esteban@Mac-mini-de-Esteban.local>
2021-12-07 20:02:53 +0100

    refactor of logic to flat action and html to a component

---
+++ src/lib/cardHour/CardHour.svelte
@@ -0,0 +1,41 @@
+<script>
+    export let hour, userHour, formattedHour, zone, price, expensive, color;
+</script>
+
+<div
+	class:hourPassed={userHour > hour}
+	class="px-4 py-2 max-w-s  bg-white rounded-xl shadow-md overflow-hidden flex flex-row items-center justify-center flex-grow"
+>
+	<div class="flex flex-row flex-1">
+		<span class="text-s leading-5 font-semibold">{formattedHour}h </span>
+		<span class="text-s leading-5 font-semibold ml-2">{zone}</span>
+	</div>
+	<div class="flex flex-row ">
+		<span
+			class=" flex text-s leading-5 font-semibold rounded-md content-around  items-center justify-center"
+			>{price} €/kWh
+		</span>
+		<span
+			class:bgGreen={color === 'green'}
+			class:bgYellow={color === 'yellow'}
+			class:bgRed={color === 'red'}
+			class="px-3 py-1 flex text-s leading-5 font-semibold rounded-md  items-center justify-center ml-2 min-w-[50px]"
+			>{expensive}
+		</span>
+	</div>
+</div>
+
+<style>
+	.bgGreen {
+		@apply bg-green-300 text-green-800 border-green-500 border-[1px];
+	}
+	.bgYellow {
+		@apply bg-yellow-300 text-yellow-800 border-yellow-500 border-[1px];
+	}
+	.bgRed {
+		@apply bg-red-300 text-red-800 border-red-500 border-[1px];
+	}
+	.hourPassed {
+		@apply opacity-40;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,7 @@
 <script context="module">
 	export const prerender = true;
 	import data from '$data/cleaned-price.json';
+	import CardHour from '$lib/cardHour/CardHour.svelte';
 </script>
 
 <script>
@@ -14,37 +15,9 @@
 		return `${formattedHour}:${formattedMinutes}h`;
 	};
 	let userTime = formatTime(userHour, userMinutes);
-
-	let newData = data.map((time) => {
-		let hour = time.hour;
-		if (!time.formattedHour) {
-			time.formattedHour = hour < 10 ? `0${hour}:00` : `${hour}:00`;
-		}
-
-		return time;
-	});
-
-	let sortedData = newData.sort(({ price: a }, { price: b }) => a - b);
-
-	for (let [index, element] of sortedData.entries()) {
-		if (index < 8) {
-			element.expensive = '€';
-			element.color = 'green';
-		} else if (index >= 8 && index < 16) {
-			element.expensive = '€€';
-			element.color = 'yellow';
-		} else {
-			element.expensive = '€€€';
-			element.color = 'red';
-		}
-	}
-
-	let orderedData = sortedData.sort(({ hour: a }, { hour: b }) => a - b);
-
-	let { price } = orderedData.find((data) => data.hour === userHour);
-
-	let nextCheapestData = orderedData.slice(userHour);
-	let [nextCheapestHour, ...rest] = nextCheapestData.sort(({ price: a }, { price: b }) => a - b);
+	let { price } = data.find((data) => data.hour === userHour);
+	let nextCheapestData = data.slice(userHour);
+	let [nextCheapestHour] = nextCheapestData.sort(({ price: a }, { price: b }) => a - b);
 </script>
 
 <svelte:head>
@@ -56,48 +29,13 @@
 
 	<p>Son las {userTime} y el precio es {price} €/kWh</p>
 	<p>
-		La hora más barata de lo que queda de dia es a las {nextCheapestHour.formattedHour}h a un precio de
+		La hora más barata de lo que queda de dia es a las {nextCheapestHour.formattedHour}h a un precio
+		de
 		{nextCheapestHour.price} €/kWh
 	</p>
 	<div class="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3">
-		{#each orderedData as { hour, formattedHour, price, zone, expensive, color }, i (hour)}
-			<div
-				class:hourPassed={userHour > hour}
-				class="px-4 py-2 max-w-s  bg-white rounded-xl shadow-md overflow-hidden flex flex-row items-center justify-center flex-grow"
-			>
-				<div class="flex flex-row flex-1">
-					<span class="text-s leading-5 font-semibold">{formattedHour}h </span>
-					<span class="text-s leading-5 font-semibold capitalize ml-2">{zone}</span>
-				</div>
-				<div class="flex flex-row ">
-					<span
-						class=" flex text-s leading-5 font-semibold rounded-md content-around  items-center justify-center"
-						>{price} €/kWh
-					</span>
-					<span
-						class:bgGreen={color === 'green'}
-						class:bgYellow={color === 'yellow'}
-						class:bgRed={color === 'red'}
-						class="px-3 py-1 flex text-s leading-5 font-semibold rounded-md  items-center justify-center ml-2 min-w-[50px]"
-						>{expensive}
-					</span>
-				</div>
-			</div>
+		{#each data as { hour, formattedHour, price, zone, expensive, color }, i (hour)}
+			<CardHour {hour} {formattedHour} {price} {zone} {expensive} {color} {userHour} />
 		{/each}
 	</div>
 </section>
-
-<style>
-	.bgGreen {
-		@apply bg-green-300 text-green-800 border-green-500 border-[1px];
-	}
-	.bgYellow {
-		@apply bg-yellow-300 text-yellow-800 border-yellow-500 border-[1px];
-	}
-	.bgRed {
-		@apply bg-red-300 text-red-800 border-red-500 border-[1px];
-	}
-	.hourPassed {
-		@apply opacity-40;
-	}
-</style>
