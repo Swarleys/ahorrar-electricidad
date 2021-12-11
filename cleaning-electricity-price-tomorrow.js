cea75909de4e40a012e7084156631f4c2b917985
Esteban Ramos Coronado <esteban@Mac-mini-de-Esteban.local>
2021-12-11 14:58:58 +0100

    feat: adding tomorrow prices

---
+++ cleaning-electricity-price-tomorrow.js
@@ -0,0 +1,45 @@
+import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.10/src/json.ts';
+
+const filename = 'electricity-price-api-tomorrow.json';
+const json = await readJSON(filename);
+
+const createZone = (hour) => {
+	if (hour >= 0 && hour < 8) {
+		return 'Valle';
+	} else if ((hour >= 8 && hour < 10) || (hour >= 14 && hour < 18) || (hour >= 22 && hour < 24)) {
+		return 'Llano';
+	} else {
+		return 'Punta';
+	}
+};
+
+const filteredData = json.PVPC.map(({ Dia, Hora, PCB }) => {
+	const getFirstHour = Hora.split('-')[0];
+	return {
+		day: Dia,
+		hour: +getFirstHour,
+		formattedHour: `${getFirstHour}:00`,
+		price: (+PCB.split(',')[0] / 1000).toFixed(3),
+		zone: createZone(+getFirstHour)
+	};
+});
+
+const sortedByPrice = filteredData.sort(({ price: a }, { price: b }) => a - b);
+
+for (let [index, element] of sortedByPrice.entries()) {
+	if (index < 8) {
+		element.expensive = '€';
+		element.color = 'green';
+	} else if (index >= 8 && index < 16) {
+		element.expensive = '€€';
+		element.color = 'yellow';
+	} else {
+		element.expensive = '€€€';
+		element.color = 'red';
+	}
+}
+
+const sortedByHour = sortedByPrice.sort(({ hour: a }, { hour: b }) => a - b);
+
+const newFilename = 'src/lib/data/cleaned-price-tomorrow.json';
+await writeJSON(newFilename, sortedByHour);
