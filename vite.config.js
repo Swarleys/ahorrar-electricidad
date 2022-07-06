55d2c75d8539d1b383043422cec7fe8bd7107767
Esteban Ramos Coronado <esteban@Mac-mini-de-Esteban.local>
2022-07-06 22:23:56 +0200

    Upgrading versions and breaking changes to vite

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
+
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			// these are the aliases and paths to them
+			$data: path.resolve('./src/lib/data'),
+			$actions: path.resolve('./src/Actions')
+		}
+	},
+	ssr: {
+		noExternal: ['chart.js']
+	}
+};
+
+export default config;
